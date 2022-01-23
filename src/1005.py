dac1da9b7550a1e636d23b1d7ec957322caadd11
ddiyoung <wsw3998@naver.com>
2022-01-23 17:18:18 +0900

    solving 1005

---
+++ src/1005.py
@@ -0,0 +1,13 @@
+def main():
+    T = int(input())
+
+    for _ in range(T):
+        N, K = map(int, input().split())
+        D = list(map(int, input().split()))
+        for _ in range(K):
+            # create graph and topological sort
+            print("GOOD")
+
+
+if __name__ == "__main__":
+    main()
