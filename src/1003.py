4d5ca9aee6299c09b944c972d5d503babffcc61a
ddiyoung <wsw3998@naver.com>
2022-01-23 15:35:05 +0900

    slove 1003 dynamic programming

--- src/1003.py
+++ src/1003.py
@@ -1,32 +1,21 @@
-from argparse import ONE_OR_MORE
-
-
-def fibonacci(n):
-    if n == 0:
-        global zero 
-        zero += 1
-        return 0
-    elif n == 1:
-        global one
-        one += 1
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-
 def main():
     N = int(input())
 
-    global zero
-    global one
-    zero = 0
-    one = 0
+    fibonacci = [0, 1, 1]
+
+    for i in range(3, 41):
+        fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
 
     for i in range(N):
-        fibonacci(int(input()))
-        print("%d %d" %(zero, one))
-        zero = 0
-        one = 0
+        T = int(input())
+
+        if T == 0:
+            print("1 0")
+        elif T == 1:
+            print("0 1")
+        else:
+            print(fibonacci[T-1], fibonacci[T])
+
 
 if __name__ == "__main__":
     main()
