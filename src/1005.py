5b6f50b756c8670c39bbf6da3bdccf428bcc3a97
ddiyoung <wsw3998@naver.com>
2022-08-04 17:50:34 +0900

    P.1005 Create Graph

--- src/1005.py
+++ src/1005.py
@@ -1,13 +1,29 @@
+class Graph:
+    #그래프
+    def __init__(self, n):
+        self.G = dict(zip([i for i in range(1, n+1)], [[] for _ in range(n)]))
+
+    def add(self, src, dst):
+        self.G[src].append(dst)
+    
+
 def main():
     T = int(input())
 
     for _ in range(T):
         N, K = map(int, input().split())
-        D = list(map(int, input().split()))
-        for _ in range(K):
-            # create graph and topological sort
-            print("GOOD")
 
+        graph = Graph(N) #N개의 정점을 가지는 그래프 생성
+
+        D = list(map(int, input().split()))
+
+        for _ in range(K): #Edges의 개수 만큼 반복
+            src, dst = map(int, input().split())
+            graph.add(src, dst)
+
+        print(graph.G)
+
+        W = int(input())
 
 if __name__ == "__main__":
     main()
