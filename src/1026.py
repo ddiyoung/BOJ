9944e2f64edda187bdc6b6c4a3f8225f987d7f1b
ddiyoung <ddiyoung@sju.ac.kr>
2023-01-12 01:05:40 +0900

    Solved 1026

---
+++ src/1026.py
@@ -0,0 +1,18 @@
+def main():
+    N = int(input())
+
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    A.sort()
+    B.sort(reverse=True)
+
+    sum = 0
+
+    for i in range(N):
+        sum += A[i] * B[i]
+    
+    print(sum)
+
+if __name__ == "__main__":
+    main()
