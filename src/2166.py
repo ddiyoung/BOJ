f04fbfb7540fab49cbe927e9e4b2cd61d8dee8a7
ddiyoung <ddiyoung@sju.ac.kr>
2023-01-12 15:12:19 +0900

    Solved 2166

---
+++ src/2166.py
@@ -0,0 +1,16 @@
+def main():
+    n = int(input())
+    x, y = [], []
+    answer = 0
+    for _ in range(n):
+        a, b = map(int,input().split())
+        x.append(a)
+        y.append(b)
+    x, y = x + [x[0]], y + [y[0]]
+
+    for i in range(n):
+        answer += (x[i]*y[i+1])-(x[i+1]*y[i])
+    print(round(abs(answer)/2,1))
+
+if __name__ == "__main__":
+    main()
